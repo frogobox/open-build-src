92a0d019dc00e94758bf508734087076f4fbd008
amirisback <faisalamircs@gmail.com>
2022-08-07 19:04:50 +0700

    Release v2.0.3

--- buildSrc/src/main/kotlin/ProjectSetting.kt
+++ buildSrc/src/main/kotlin/ProjectSetting.kt
@@ -25,11 +25,11 @@ object ProjectSetting {
 
     const val VERSION_MAJOR = 2
     const val VERSION_MINOR = 0
-    const val VERSION_PATCH = 2
+    const val VERSION_PATCH = 3
 
     // -----------------------------------------------------------------------------------------------------------------
 
-    const val KOTLIN_VERSION = "1.7.0"
+    const val KOTLIN_VERSION = "1.6.10"
 
     // -----------------------------------------------------------------------------------------------------------------
 
