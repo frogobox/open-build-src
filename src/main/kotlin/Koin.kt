271c82a290cce10fd2aef5e7ee7b313e17209f6d
amirisback <faisalamircs@gmail.com>
2022-12-10 11:54:55 +0700

    Release v2.0.7

--- src/main/kotlin/Koin.kt
+++ src/main/kotlin/Koin.kt
@@ -18,23 +18,27 @@ object Koin {
     fun core(version: String): String = "io.insert-koin:koin-core:$version"
 
     // Koin main features for Android (Scope,ViewModel ...)
-    const val android = "io.insert-koin:koin-android:${Version.Util.koin}"
+    const val android = "io.insert-koin:koin-android:${Version.Util.koin_android}"
     fun android(version: String): String = "io.insert-koin:koin-android:$version"
 
     // Koin Java Compatibility
-    const val androidCompat = "io.insert-koin:koin-android-compat:${Version.Util.koin}"
+    const val androidCompat = "io.insert-koin:koin-android-compat:${Version.Util.koin_android}"
     fun androidCompat(version: String): String = "io.insert-koin:koin-android-compat:$version"
 
     // Koin for Jetpack WorkManager
-    const val androidxWorkManager = "io.insert-koin:koin-androidx-workmanager:${Version.Util.koin}"
+    const val androidxWorkManager = "io.insert-koin:koin-androidx-workmanager:${Version.Util.koin_android}"
     fun androidxWorkManager(version: String): String = "io.insert-koin:koin-androidx-workmanager:$version"
 
+    // Koin for Navigation Graph
+    const val androidxNavigationGraph = "io.insert-koin:koin-androidx-navigation:${Version.Util.koin_android}"
+    fun androidxNavigationGraph(version: String): String = "io.insert-koin:koin-androidx-navigation:$version"
+
     // Koin for Jetpack Compose
-    const val androidxCompose = "io.insert-koin:koin-androidx-compose:${Version.Util.koin}"
+    const val androidxCompose = "io.insert-koin:koin-androidx-compose:${Version.Util.koin_android_compose}"
     fun androidxCompose(version: String): String = "io.insert-koin:koin-androidx-compose:$version"
 
     // Koin for Ktor
-    const val ktor = "io.insert-koin:koin-ktor:${Version.Util.koin}"
+    const val ktor = "io.insert-koin:koin-ktor:${Version.Util.koin_ktor}"
     fun ktor(version: String): String = "io.insert-koin:koin-ktor:$version"
 
     // Testing
--- src/main/kotlin/Version.kt
+++ src/main/kotlin/Version.kt
@@ -20,8 +20,8 @@ object Version {
     }
 
     object Ad {
-        const val unityAds = "4.4.0" // https://developers.google.com/admob/android/mediation/unity#step_3_import_the_unity_ads_sdk_and_adapter
-        const val startIo = "4.10.0" // https://support.start.io/hc/en-us/articles/360014774799-Integration-via-Maven
+        const val unityAds = "4.4.1" // https://developers.google.com/admob/android/mediation/unity#step_3_import_the_unity_ads_sdk_and_adapter
+        const val startIo = "4.10.5" // https://support.start.io/hc/en-us/articles/360014774799-Integration-via-Maven
     }
 
     object Androidx {
@@ -38,25 +38,25 @@ object Version {
         const val liveCycleArch = "2.1.0" // https://developer.android.com/jetpack/androidx/releases/lifecycle
         const val room = "2.4.3" // https://developer.android.com/jetpack/androidx/releases/room
         const val work = "2.7.1" // https://developer.android.com/jetpack/androidx/releases/work
-        const val activityKtx = "1.6.0-rc02" // https://developer.android.com/jetpack/androidx/releases/activity
-        const val fragmentKtx = "1.5.2" // https://developer.android.com/jetpack/androidx/releases/fragment
-        const val navigationKtx = "2.5.2" // https://developer.android.com/jetpack/androidx/releases/navigation
+        const val activityKtx = "1.6.1" // https://developer.android.com/jetpack/androidx/releases/activity
+        const val fragmentKtx = "1.5.5" // https://developer.android.com/jetpack/androidx/releases/fragment
+        const val navigationKtx = "2.5.3" // https://developer.android.com/jetpack/androidx/releases/navigation
         const val recyclerView = "1.2.1" // https://developer.android.com/jetpack/androidx/releases/recyclerview
         const val recyclerViewSelection = "1.1.0" // https://developer.android.com/jetpack/androidx/releases/recyclerview
-        const val camerax = "1.1.0" // https://developer.android.com/jetpack/androidx/releases/camera#kts
+        const val camerax = "1.2.0" // https://developer.android.com/jetpack/androidx/releases/camera#kts
         const val biometric = "1.1.0" // https://developer.android.com/jetpack/androidx/releases/biometric
-        const val annotation = "1.4.0" // https://developer.android.com/jetpack/androidx/releases/annotation
-        const val annotationExperimental = "1.2.0" // https://developer.android.com/jetpack/androidx/releases/annotation
+        const val annotation = "1.5.0" // https://developer.android.com/jetpack/androidx/releases/annotation
+        const val annotationExperimental = "1.5.0" // https://developer.android.com/jetpack/androidx/releases/annotation
         const val swipeRefreshLayout = "1.1.0" // https://developer.android.com/jetpack/androidx/releases/swiperefreshlayout
 
         // Compose Version
-        const val composeActivity = "1.6.0" // https://androidx.tech/artifacts/activity/activity-compose/
-        const val composeCompiler = "1.3.1" // https://developer.android.com/jetpack/androidx/releases/compose
-        const val compose = "1.2.1" // https://developer.android.com/jetpack/androidx/releases/compose
+        const val composeActivity = "1.6.1" // https://androidx.tech/artifacts/activity/activity-compose/
+        const val composeCompiler = "1.3.2" // https://developer.android.com/jetpack/androidx/releases/compose
+        const val compose = "1.3.2" // https://developer.android.com/jetpack/androidx/releases/compose
         const val composeMaterial3 = "1.0.0-alpha14" // https://developer.android.com/jetpack/androidx/releases/compose
-        const val composeNavigation = "2.5.2" // https://androidx.tech/artifacts/navigation/navigation-compose/
+        const val composeNavigation = "2.5.3" // https://androidx.tech/artifacts/navigation/navigation-compose/
         const val composeHiltNavigation = "1.0.0" // https://developer.android.com/jetpack/androidx/releases/hilt
-        const val composeMultiPlatform = "1.1.1" // https://github.com/JetBrains/compose-jb
+        const val composeMultiPlatform = "1.2.1" // https://github.com/JetBrains/compose-jb
 
         // TestImplementation
         const val junit4 = "4.13.2" // https://junit.org/junit4/
@@ -68,18 +68,18 @@ object Version {
     }
 
     object Google {
-        const val accompanist = "0.26.1-alpha" // https://github.com/google/accompanist
-        const val material = "1.8.0-alpha01" // https://github.com/material-components/material-components-android/releases
-        const val gson = "2.9.1" // https://github.com/google/gson
-        const val admob = "21.2.0" // https://developers.google.com/admob/android/sdk
+        const val accompanist = "0.28.0" // https://github.com/google/accompanist
+        const val material = "1.8.0-beta01" // https://github.com/material-components/material-components-android/releases
+        const val gson = "2.10" // https://github.com/google/gson
+        const val admob = "21.3.0" // https://developers.google.com/admob/android/sdk
         const val userMessagingPlatform = "2.0.0" // https://developers.google.com/admob/ump/android/quick-start#install_with_gradle
-        const val hilt = "2.44" // https://dagger.dev/hilt/gradle-setup
-        const val adMediationUnityAd = "4.4.0.0" // https://developers.google.com/admob/android/mediation/unity#step_3_import_the_unity_ads_sdk_and_adapter
+        const val hilt = "2.44.2" // https://dagger.dev/hilt/gradle-setup
+        const val adMediationUnityAd = "4.4.1.0" // https://developers.google.com/admob/android/mediation/unity#step_3_import_the_unity_ads_sdk_and_adapter
     }
 
     object JetBrains {
         // https://developer.android.com/jetpack/androidx/releases/compose-kotlin - Compability For Compose Version
-        const val kotlin = "1.7.10" // https://kotlinlang.org/docs/releases.html#release-details
+        const val kotlin = "1.7.20" // https://kotlinlang.org/docs/releases.html#release-details
         const val coroutines = "1.6.4" // https://github.com/Kotlin/kotlinx.coroutines
     }
 
@@ -87,15 +87,20 @@ object Version {
         const val okHttp = "4.10.0" // https://square.github.io/okhttp/ - https://search.maven.org/search?q=g:com.squareup.okhttp3
         const val retrofit = "2.9.0" // https://github.com/square/retrofit
 
-        const val rxAndroid3 = "3.0.0" // https://github.com/ReactiveX/RxAndroid
+        const val rxAndroid3 = "3.0.2" // https://github.com/ReactiveX/RxAndroid
         const val rxKotlin3 = "3.0.1" // https://github.com/ReactiveX/RxKotlin
         const val rxJava3 = "3.1.5" // https://github.com/ReactiveX/RxJava
 
-        const val glide = "4.13.2" // https://github.com/bumptech/glide
+        const val glide = "4.14.2" // https://github.com/bumptech/glide
         const val chucker = "3.5.2" // https://github.com/ChuckerTeam/chucker
-        const val coil = "2.1.0" // https://coil-kt.github.io/coil/compose/
+        const val coil = "2.2.2" // https://coil-kt.github.io/coil/compose/
+
         const val koin = "3.2.2" // https://github.com/InsertKoinIO/koin
-        const val ktor = "2.1.1" // https://ktor.io/
+        const val koin_android = "3.3.0" // https://github.com/InsertKoinIO/koin
+        const val koin_android_compose = "3.3.0" // https://github.com/InsertKoinIO/koin
+        const val koin_ktor = "3.2.2" // https://github.com/InsertKoinIO/koin
+
+        const val ktor = "2.2.1" // https://ktor.io/
         const val piracyChecker = "1.2.8" // https://github.com/javiersantos/PiracyChecker
         const val customActivityOnCrash = "2.4.0" // https://github.com/Ereza/CustomActivityOnCrash
     }
